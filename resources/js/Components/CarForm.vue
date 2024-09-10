2b42d028e3a3f2a3f7bea58df9d402a9bb7fa213
DamynFilipuzzi <damynfilipuzzi@gmail.com>
2024-09-10 09:58:52 -0600

    added style

--- resources/js/Components/CarForm.vue
+++ resources/js/Components/CarForm.vue
@@ -25,24 +25,29 @@ const submit = () => {
 <h1 class="block text-2xl text-center">Create A Car</h1>
 <form @submit.prevent="submit">
     <div>
-        <label for="model_name">Model Name</label>
-        <TextInput id="model_name" type="text" v-model="form.model_name" required />
+        <label for="model_name">Model Name<span class="text-red-500">*</span></label>
+        <TextInput id="model_name" type="text" v-model="form.model_name" required  />
+        <InputError class="mt-2" :message="form.errors.model_name" />
     </div>
     <div>
-        <label for="brand">Brand</label>
+        <label for="brand">Brand<span class="text-red-500">*</span></label>
         <TextInput id="brand" type="text" v-model="form.brand" required />
+        <InputError class="mt-2" :message="form.errors.brand" />
     </div>
     <div>
-        <label for="color">Color</label>
+        <label for="color">Color<span class="text-red-500">*</span></label>
         <TextInput id="color" type="text" v-model="form.color" required />
+        <InputError class="mt-2" :message="form.errors.color" />
     </div>
     <div>
-        <label for="year">Year</label>
+        <label for="year">Year<span class="text-red-500">*</span></label>
         <TextInput id="year" type="number" v-model="form.year" required />
+        <InputError class="mt-2" :message="form.errors.year" />
     </div>
     <div>
-        <label for="release_date">Release Date</label>
+        <label for="release_date">Release Date<span class="text-red-500">*</span></label>
         <TextInput id="release_date" type="date" v-model="form.release_date" required />
+        <InputError class="mt-2" :message="form.errors.release_date" />
     </div>
     <div class="text-right mt-2">
         <button type="submit" class="bg-blue-500 rounded-md p-2 hover:bg-blue-400 text-white">Submit</button>
