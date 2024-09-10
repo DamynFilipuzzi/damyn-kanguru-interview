8fc463a9151145507b7d731d7b8611eaa60fab77
DamynFilipuzzi <damynfilipuzzi@gmail.com>
2024-09-10 09:11:30 -0600

    added car migration/model/controller

---
+++ database/migrations/2024_09_10_150651_create_cars_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('model_name');
+            $table->string('brand')->unique();
+            $table->string('color', length: 40);
+            $table->integer('year');
+            $table->date('release_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
